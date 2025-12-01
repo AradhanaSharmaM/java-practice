a071152d10c48370bfc33631e75a7868de33bf62
aradhana.sharma <aradhana.sharma@pharmeasy.in>
2025-12-01 16:36:51 +0530

    Day 1 - Java introduction and syntax

--- Day_One.java
+++ Day_One.java
@@ -1,6 +1,12 @@
 public class Day_One{
     public static void main(String[] args){
+        System.out.println("Hey there! What's up$ide down");
         System.out.println("First day - 01/12/2025");
         System.out.println("Starting with java basics today");
+        System.out.println(2+5);
+        System.out.println(10000*76576565);
+        //This is single line comment
+        /*this is multi line comment
+        yaay........!!!!!!! */
     }
 }
