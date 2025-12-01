ac8251fd216c4cf97d850e0b5407b20ffd9b1d46
aradhana.sharma <aradhana.sharma@pharmeasy.in>
2025-12-01 16:19:18 +0530

    Day 1 - Java introduction and syntax

---
+++ Day_One.java
@@ -0,0 +1,6 @@
+public class Day_One{
+    public static void main(String[] args){
+        System.out.println("First day - 01/12/2025");
+        System.out.println("Starting with java basics today");
+    }
+}
